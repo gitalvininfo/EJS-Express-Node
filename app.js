9dbe79799ccc44fedef5a8efeae0d2748774a30b
Alvin Yanson <yansonalvin29@gmail.com>
2021-10-31 14:30:34 +0800

    finish

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = 5000
@@ -12,13 +13,16 @@ app.use('/js', express.static(__dirname + 'public/js'))
 
 
 // Templating engine
-app.set('views', './public/src/views/partials')
+app.set('views', './public/src/views')
 app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 // Routes
 const newsRouter = require('./public/src/routes/news');
 app.use('/', newsRouter);
+app.use('/article', newsRouter);
 
 
 // Listen to port
--- public/src/routes/news.js
+++ public/src/routes/news.js
@@ -1,18 +1,18 @@
 const express = require('express')
-const newsRouter = express.Router();
+const newsRouter = express.Router()
 const axios = require('axios')
 
+
 newsRouter.get('', async (req, res) => {
-
     try {
-
         const newsAPI = await axios.get(`https://raddy.co.uk/wp-json/wp/v2/posts`)
 
         res.render('news', { articles: newsAPI.data })
 
-
     } catch (err) {
         if (err.response) {
+            res.render('news', { articles: null })
+
             console.error(err.response.data)
             console.error(err.response.status)
             console.error(err.response.headers)
@@ -22,14 +22,51 @@ newsRouter.get('', async (req, res) => {
             console.error(`Error, ${err.message}`)
         }
     }
+})
 
+newsRouter.get('/:id', async (req, res) => {
+    let articleId = req.params.id;
+    try {
+        const newsAPI = await axios.get(`https://raddy.co.uk/wp-json/wp/v2/posts/${articleId}`)
 
+        res.render('newsSingle', { article: newsAPI.data })
 
+    } catch (err) {
+        if (err.response) {
+            res.render('newsSingle', { article: null })
 
+            console.error(err.response.data)
+            console.error(err.response.status)
+            console.error(err.response.headers)
+        } else if (err.request) {
+            console.error(err.request)
+        } else {
+            console.error(`Error, ${err.message}`)
+        }
+    }
+})
 
+newsRouter.post('', async (req, res) => {
+    let search = req.body.search;
 
+    try {
+        const newsAPI = await axios.get(`https://raddy.co.uk/wp-json/wp/v2/posts?search=${search}`)
 
+        res.render('newsSearch', { articles: newsAPI.data })
 
+    } catch (err) {
+        if (err.response) {
+            res.render('newsSearch', { articles: null })
+
+            console.error(err.response.data)
+            console.error(err.response.status)
+            console.error(err.response.headers)
+        } else if (err.request) {
+            console.error(err.request)
+        } else {
+            console.error(`Error, ${err.message}`)
+        }
+    }
 })
 
 module.exports = newsRouter;
