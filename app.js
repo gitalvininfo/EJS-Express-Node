202181b419332511e9d4f0f8e31ae0ea21d7801e
Alvin Yanson <yansonalvin29@gmail.com>
2021-10-31 12:31:28 +0800

    watch scss

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
-const port = 5000;
+const app = express()
+const port = 5000
 
+
+// Static files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/img', express.static(__dirname + 'public/img'))
+app.use('/js', express.static(__dirname + 'public/js'))
+
+
+// Templating engine
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
+
+
+// Routes
+const newsRouter = require('./src/routes/news');
+app.use('/', newsRouter);
+
+
+// Listen to port
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on port ${port}`)
 })
